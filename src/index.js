e2e86cb88eeb31394a6cad1dda7c3b106f8f6c9e
jatomic <johnatomich@gmail.com>
2022-02-04 22:32:02 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { arc } from 'd3';
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
